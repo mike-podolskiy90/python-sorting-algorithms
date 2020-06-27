8ca01bf35f810262837ad1b7788594507c7fc193
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-06-27 23:59:24 +0200

    Merge sort, update readme

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Sorting algorithms
 
-1. Quick sort
+## Algorithms
+
+1. [Quicksort](https://en.wikipedia.org/wiki/Quicksort)
+
+    [Lomuto partition](quick-sort/quick-sort-lomuto.py)
+    
+    [Hoare partition](quick-sort/quick-sort-hoare.py)
+    
+2. [Mergesort](https://en.wikipedia.org/wiki/Merge_sort)
+
+3. [Timsort](https://en.wikipedia.org/wiki/Timsort)
+
+4. [Heapsort](https://en.wikipedia.org/wiki/Heapsort)
+
+5. [Bubble sort](https://en.wikipedia.org/wiki/Bubble_sort)
+
+## Links
+
+https://www.bigocheatsheet.com/
---
+++ merge-sort/merge-sort.py
@@ -0,0 +1,41 @@
+arr_to_sort = [66, 67, 22, 84, 1, 3, 7, 17, 34, 24, 99, 76, 57]
+print(arr_to_sort)
+
+
+def merge(arr, left, right):
+    i = j = k = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        middle = len(arr) // 2
+
+        left = arr[:middle]
+        right = arr[middle:]
+
+        merge_sort(left)
+        merge_sort(right)
+        merge(arr, left, right)
+
+
+merge_sort(arr_to_sort)
+print(arr_to_sort)
