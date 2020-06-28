425de39f01bf591b98487199cc0b52d88eb302c1
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-06-28 02:00:55 +0200

    Bubble sort

---
+++ bubble-sort/bubble-sort-regular.py
@@ -0,0 +1,15 @@
+arr_to_sort = [66, 67, 22, 84, 1, 3, 7, 17, 34, 24, 99, 76, 57]
+print(arr_to_sort)
+
+
+def bubble_sort(arr):
+    n = len(arr)
+
+    for j in range(n):
+        for i in range(n - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+
+bubble_sort(arr_to_sort)
+print(arr_to_sort)
